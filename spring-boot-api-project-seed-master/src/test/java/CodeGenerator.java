cdaef077b14d081ccaa109ae48f59470c414e7fb
gbrpc <49590980+gbrpc@users.noreply.github.com>
2020-04-29 00:08:42 +0800

    Update CodeGenerator.java

--- spring-boot-api-project-seed-master/src/test/java/CodeGenerator.java
+++ spring-boot-api-project-seed-master/src/test/java/CodeGenerator.java
@@ -20,7 +20,7 @@ public class CodeGenerator {
     //JDBC配置，请修改为你项目的实际配置
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test";
     private static final String JDBC_USERNAME = "root";
-    private static final String JDBC_PASSWORD = "root";
+    private static final String JDBC_PASSWORD = "Root";
     private static final String JDBC_DIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
 
     private static final String PROJECT_PATH = System.getProperty("user.dir");//项目在硬盘上的基础路径
@@ -35,7 +35,6 @@ public class CodeGenerator {
 
     private static final String AUTHOR = "CodeGenerator";//@author
     private static final String DATE = new SimpleDateFormat("yyyy/MM/dd").format(new Date());//@date
-    //private static final String DATE = new SimpleDateFormat("yyyy/MM/dd").format(new Date());//@date
 
     public static void main(String[] args) {
         genCode("输入表名");
